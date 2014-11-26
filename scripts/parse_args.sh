446839761a120862bcff1aa40c8a89ac6b309063
John Duddy <jduddy@illumina.com>
2014-11-26 14:06:22 -0800

    First successful test of fastq + parallel assembly

--- scripts/parse_args.sh
+++ scripts/parse_args.sh
@@ -4,48 +4,55 @@
 # RUN_DIR
 # OUT_DIR
 # SCRATCH_DIR
-# CLEANUP_SCRATCH
 
 usage() { echo "Usage: $0 -r run_dir -o output_directory [-s scratch_root]" 1>&2; exit 1; }
 
 
+SCRATCH_ROOT=/scratch
+
+
+while getopts "r:o:s:" o; do
+	case "${o}" in
+		r)
+			RUN_DIR=$(readlink -m ${OPTARG})
+			;;
+		o)
+			OUT_DIR=$(readlink -m ${OPTARG})
+			;;
+		s)
+			SCRATCH_ROOT=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$OUT_DIR"  || -z "$RUN_DIR" ]]; then
+	usage
+fi
+if [[ $RUN_DIR =~ $OUT_DIR || $RUN_DIR =~ $OUT_DIR/.* || $OUT_DIR =~ $RUN_DIR/.* ]]
+then
+	echo "Input and output directories must not overlap"
+	exit 1
+fi
+
+if ! [ -d "$OUT_DIR" ] && ! mkdir -p "$OUT_DIR"
+then
+	echo "Unable to make output directory"
+	usage
+fi
+
+if ! [ -f "$RUN_DIR/SampleSheet.csv" ]
+then
+	echo "Run or SampleSheet.csv do not exist"
+	exit 1
+fi
+
+
 if [ -z "$SCRATCH_DIR" ]
 then
-	SCRATCH_ROOT=/scratch
-	CLEANUP_SCRATCH=true
-
-
-	while getopts "r:o:s:" o; do
-		case "${o}" in
-			r)
-				export RUN_DIR=$(readlink -e ${OPTARG})
-				;;
-			o)
-				export OUT_DIR=${OPTARG}
-				;;
-			s)
-				export SCRATCH_ROOT=${OPTARG}
-				;;
-			*)
-				usage
-				;;
-		esac
-	done
-	shift $((OPTIND-1))
-
-	if [ -z "$OUT_DIR" ]; then
-		usage
-	fi
-
-	if ! [ -d "$OUT_DIR" ] || ! mkdir -p "$OUT_DIR"
-	then
-		echo "Unable to make output directory"
-		usage
-	fi
-	# make absolute path in case someone changes the directory
-	export OUT_DIR=$(readlink -e $OUT_DIR)
-
-
 	if ! [ -z "$JOB_ID" ]
 	then
 		export SCRATCH_DIR=$(echo $SCRATCH_ROOT/${JOB_ID}*)
--- scripts/samplesheet.sh
+++ scripts/samplesheet.sh
@@ -14,7 +14,7 @@ Workflow,GenerateFastq
 
 [Settings],,,,,,,,
 ReverseComplement,1
-Adapter,CTGTCTCTTATACACATCT+AGATGTGTATAAGAGACAG
+Adapter,CTGTCTCTTATACACATCT+AGATGTGTATAAGAGACAG+GATCGGAAGAGCACACGTCTGAACTCCAGTCAC+GATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
 " > $SRCDEST
 # copy the [Reads] and [Data] sections verbatim
 	local in_section=0
@@ -37,4 +37,22 @@ Adapter,CTGTCTCTTATACACATCT+AGATGTGTATAAGAGACAG
 		fi
 	done < $TMPFILE
 	rm -f $TMPFILE
+}
+
+get_sample_genome()
+{
+	echo $*
+	local sample_sheet=$1
+	local sample=$2
+	local sample_line=$(grep -i $sample $sample_sheet | tr "," "\n" | grep -i WholeGenome | head -1 | tr '\\' '/')
+	if ! [ -z "$sample_line" ]
+	then
+		if [ -f $sample_line/genome.fa ]
+		then
+			echo $sample_line/genome.fa
+		elif [ -f /illumina/development/Isis/Genomes/$sample_line/genome.fa ]
+		then
+			echo /illumina/development/Isis/Genomes/$sample_line/genome.fa
+		fi
+	fi
 }
