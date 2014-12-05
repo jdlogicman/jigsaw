d9b3b90a9d3325591b4ebe8421b51deac93433c5
John Duddy <jduddy@illumina.com>
2014-12-05 14:08:58 -0800

    Downsample Fastqs to 70 mean, go parallel

---
+++ scripts/tool_paths.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SPADES_DIR=/illumina/thirdparty/spades/SPAdes-3.1.1-Linux/bin
+QUAST_DIR=/illumina/thirdparty/quast-2.3
+PYTHON_DIR=/illumina/thirdparty/python/python-2.7.8/bin
+export PYTHONPATH_DIR=/illumina/thirdparty/python/python_libraries/packages:$PYTHONPATH
+
+PICARD_DIR=/illumina/thirdparty/picard-tools/picard-tools-1.85
+MUMMER_DIR=$QUAST_DIR/libs/MUMmer3.23-linux
+LAST_DIR=/illumina/thirdparty/last
+SAMTOOLS_DIR=/illumina/thirdparty/samtools/latest
+BEDTOOLS_DIR=/illumina/thirdparty/bedtools/bedtools2-2.20.1/bin
+FASTQ_SAMPLER_DIR=/illumina/scratch/Jigsaw/tools/FastqSampler
