49f8d09589311c754868eae9d987864555362a67
John Duddy <jduddy@illumina.com>
2014-12-19 15:23:27 -0800

    Replace genome paths with the latest version Do not trim reads if the reads are already shorter than the specified value

--- scripts/samplesheet.sh
+++ scripts/samplesheet.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+replace_genome_paths()
+{
+# input - stdin
+# output - stdout
+	sed 's/\\/\//g' |  \
+	sed 's/WholeGenomeFASTA/WholeGenomeFasta/g' | \
+	sed 's/Escherichia_coli_K_12_MG1655\/NCBI\/2001-10-15/Escherichia_coli_K_12_MG1655\/NCBI\/2013-09-26/g' |  \
+	sed 's/Rhodobacter_sphaeroides_2.4.1\/NCBI\/2005-10-07/Rhodobacter_sphaeroides_2.4.1\/NCBI\/2012-06-25/g' 
+}
+
 rewrite_samplesheet_for_alignment()
 {
 # $1 = source & target sample sheet
@@ -32,7 +42,8 @@ svannotation,None
 indelrealignment,None
 Adapter,CTGTCTCTTATACACATCT+AGATGTGTATAAGAGACAG+GATCGGAAGAGCACACGTCTGAACTCCAGTCAC+GATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
 " > $SRCDEST
-# copy the [Reads] and [Data] sections verbatim
+# copy the [Reads] and [Data] sections 
+# along the way, replace any genomes with the proper versions of them
 	local in_section=0
 
 	while read -r line
@@ -49,7 +60,7 @@ Adapter,CTGTCTCTTATACACATCT+AGATGTGTATAAGAGACAG+GATCGGAAGAGCACACGTCTGAACTCCAGTCA
 		fi
 		if [ $in_section == 1 ]
 		then
-			echo -e "$line" >> $SRCDEST
+	echo -e "$line" | replace_genome_paths >> $SRCDEST
 		fi
 	done < $TMPFILE
 	rm -f $TMPFILE
@@ -62,11 +73,7 @@ get_sample_genome()
 	local sample_line=$(grep -i $sample $sample_sheet | tr "," "\n" | grep -i WholeGenome | head -1 | tr '\\' '/' | sed 's/FASTA/Fasta/g')
 	if ! [ -z "$sample_line" ]
 	then
-		# JED - automatically change EColi to new reference
-		if [[ $sample_line =~ 1655/NCBI/2001 ]]
-		then
-			sample_line="Escherichia_coli_K_12_MG1655/NCBI/2013-09-26/Sequence/WholeGenomeFasta"
-		fi
+		sample_line=$(echo $sample_line | replace_genome_paths)
 		if [ -f $sample_line/genome.fa ]
 		then
 			echo $sample_line/genome.fa
