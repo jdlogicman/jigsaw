0e5d6b3888bece2d6a98e24c58c7a9c78dd539f4
John Duddy <jduddy@illumina.com>
2015-01-13 13:23:46 -0800

    rudimentary filtering

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
 var app = angular.module('main', ['ngTable'])
-	.controller('DemoCtrl', function($scope, $http) {
+	.controller('DemoCtrl', function($scope, $filter, ngTableParams) {
 			$scope.results = jigsawresults;
+			$scope.tableParams = new ngTableParams({
+				page: 1,            // show first page
+				count: 10,          // count per page
+				sorting: {
+				    name: 'asc'     // initial sorting
+				}
+			    },{
+				total: $scope.results.length, // length of $scope.results
+				getData: function($defer, params) {
+				    // use build-in angular filter
+				    var orderedData = params.sorting() ?
+							$filter('orderBy')($scope.results, params.orderBy()) :
+							$scope.results;
+
+				    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+				}
+			    });
 		}
 	);
 
