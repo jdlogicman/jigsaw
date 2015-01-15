84302b504c2f00e6a37541e1d9269d0e24246f53
John Duddy <jduddy@illumina.com>
2015-01-15 13:31:57 -0800

    Got sorting & filtering working!

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 var app = angular.module('main', ['ngTable'])
 	.controller('DemoCtrl', function($scope, $filter, ngTableParams) {
-			$scope.results = jigsawresults;
+			// $scope.results = jigsawresults;
+			$scope.genomeFilter='Escherichia';
 			$scope.tableParams = new ngTableParams({
 				page: 1,            // show first page
 				count: 10,          // count per page
 				sorting: {
-				    name: 'asc'     // initial sorting
+				    ANALYSIS_ID: 'desc'     // initial sorting
 				}
 			    },{
-				total: $scope.results.length, // length of $scope.results
+				total: jigsawresults.length, // length of $scope.results
 				getData: function($defer, params) {
 				    // use build-in angular filter
 				    var orderedData = params.sorting() ?
-							$filter('orderBy')($scope.results, params.orderBy()) :
-							$scope.results;
+							$filter('orderBy')(jigsawresults, params.orderBy()) :
+							jigsawresults;
 
 				    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 				}
