50b8b8f244d89b643f4725c23396ae403f258e52
John Duddy <jduddy@illumina.com>
2015-02-20 16:46:27 -0800

    convert to use FastqToolkit for downsampling

--- scripts/tool_paths.sh
+++ scripts/tool_paths.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SPADES_DIR=/illumina/thirdparty/spades/SPAdes-3.1.1-Linux/bin
+#SPADES_DIR=/illumina/thirdparty/spades/SPAdes-3.5.0-Linux/bin
+FASTQ_TOOLKIT=/illumina/scratch/Jigsaw/tools/FastqToolkit/latest
 QUAST_DIR=/illumina/thirdparty/quast-2.3
 PYTHON_DIR=/illumina/thirdparty/python/python-2.7.8/bin
 export PYTHONPATH_DIR=/illumina/thirdparty/python/python_libraries/packages:$PYTHONPATH
@@ -10,5 +12,4 @@ MUMMER_DIR=$QUAST_DIR/libs/MUMmer3.23-linux
 LAST_DIR=/illumina/thirdparty/last
 SAMTOOLS_DIR=/illumina/thirdparty/samtools/latest
 BEDTOOLS_DIR=/illumina/thirdparty/bedtools/bedtools2-2.20.1/bin
-FASTQ_SAMPLER_DIR=/illumina/scratch/Jigsaw/tools/FastqSampler
 BWA_DIR=/illumina/thirdparty/bwa/latest
