0fe87b494b7e4f1311faf5a02ce8aca1a41b2059
John Duddy <jduddy@illumina.com>
2015-02-25 13:19:54 -0800

    Column chooser working

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ var app = angular.module('main', ['ngTable'])
 	    };
 	})
 	.controller('DemoCtrl', function($scope, $filter, ngTableParams) {
+			// get unique list of genomes
 			var u = {}, a = [];
 			for(var i = 0, l = jigsawresults.length; i < l; ++i){
 				if(!u.hasOwnProperty(jigsawresults[i].SUMMARYReferenceGenome)) {
@@ -12,12 +13,42 @@ var app = angular.module('main', ['ngTable'])
 				}
 			    }
 			$scope.genomes = a;
+			// apply genome filter
 			for (var index=0; index < a.length; index++) {
 				if (a[index].indexOf('Esch') == 0) {
 					$scope.genomeFilter = a[index];
 					break;
 				}
 			}
+			$scope.columns = [
+				{ title: 'Analysis ID', field:'ANALYSIS_ID', visible: true },
+				{ title: 'Sample ID', field:'SAMPLE_ID', visible: true },
+				{ title: 'genome %', field:'QUASTbrokenGenomefraction', visible: true },
+				{ title: '#contigs', field:'QUASTbrokencontigs', visible: true },
+				{ title: '#misassemblies', field:'QUASTbrokenmisassemblies', visible: true },
+				{ title: 'Largest Alignment', field:'QUASTLargestalignment', visible: true },
+				{ title: 'Median Insert Size', field:'FIREBRANDinsert_size_median', visible: true },
+				{ title: 'Insert StdDev', field:'FIREBRANDinsert_size_stdev', visible: true },
+				{ title: 'Mean Read Len', field:'FIREBRANDmean_read_length', visible: true },
+				{ title: 'NGA50', field:'QUASTbrokenNGA50', visible: true },
+			    ];
+			 var u = {};
+			for (var x in $scope.columns) {
+				u[$scope.columns[x].field] = 1;
+			}
+			console.log(u);
+			if (jigsawresults.length > 0) {
+				for (var propertyName in jigsawresults[0]) {
+					if (!u.hasOwnProperty(propertyName)) {
+						u[propertyName] = 1;
+						$scope.columns.push({ title: propertyName, field:propertyName, visible: false});
+					}
+				}
+			}
+			
+
+			$scope.genomes = a;
+			$scope.showColumnChooser = false;
 			$scope.tableParams = new ngTableParams({
 				page: 1,            // show first page
 				count: 100,          // count per page
